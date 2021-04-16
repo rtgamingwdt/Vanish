5e802f9cb2e2df7f597b039616a18bc711cf5447
RT <46906497+rtgamingwdt@users.noreply.github.com>
2021-04-16 03:58:21 -0400

    Create Main.php

---
+++ src/rtgamingwdt/Vanish/Main.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace rtgamingwdt\Vanish;
+
+use pocketmine\Player;
+use pocketmine\Server;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+use pocketmine\event\Listener;
+
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase {
+  
+  public function onEnable() {
+    
+  }
+  
+  public function onDisable() {
+    
+  }
+  
+  public function onCommand(CommandSender $sender, Command $cmd, String label, Array $args) : bool {
+    if($sender instanceof Player) {
+      
+      Player $player = $sender->getPlayer();
+      
+      if($label === "vanish") {
+        Player online = Server.getOnlinePlayers();
+        online.hidePlayer(player);
+        $sender->sendMessage(TextFormat::GREEN."You have successfully vanished!.");
+        return true;
+      }
+    } else {
+      $sender->sendMessage(TextFormat::RED."Only players can use this command.");
+      return true;
+    }
+    return false;
+  }
+}
